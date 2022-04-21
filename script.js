b0e1c71a16971e47700a862996c0e917ce8f3ea0
Sean Kennedy <snkennedy21@gmail.com>
2022-04-21 15:57:41 -0400

    add classes

--- script.js
+++ script.js
@@ -1,38 +1,89 @@
 "use strict";
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+const formContainer = document.querySelector(".form-container");
+const formInputName = document.querySelector(".form__input--name");
+const formInputType = document.querySelector(".form__input--type");
+const starWidgets = document.querySelectorAll(".star-widget__input");
+const formTextArea = document.querySelector(".form__text-area");
+const form = document.querySelector(".form");
 
-      const coords = [latitude, longitude];
-      const map = L.map("map").setView(coords, 13);
+class Restaurant {
+  date = new Date();
+  id = String(Date.now()).slice(-10);
 
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  constructor(name, type, rating, notes) {
+    this.name = name;
+    this.type = type;
+    this.rating = rating;
+    this.notes = notes;
+  }
+}
 
-      map.on("click", function (mapEvent) {
-        console.log(mapEvent);
-        const lat = mapEvent.latlng.lat;
-        const lng = mapEvent.latlng.lng;
+const tonyB = new Restaurant(`tonyB`, `pizza`, 5, `yes`);
+console.log(tonyB);
 
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnclick: false,
-            })
-          )
-          .openPopup();
-      });
-    },
-    function () {
-      alert("could not get position");
-    }
-  );
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener("submit", this._newRestaurant.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("could not get position");
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const coords = [latitude, longitude];
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    formContainer.classList.remove("hidden");
+    formInputName.focus();
+  }
+
+  _newRestaurant(e) {
+    e.preventDefault();
+    formInputName.value = formInputType.value = formTextArea.value = "";
+    starWidgets.forEach((widget) => {
+      widget.checked = false;
+    });
+    const lat = this.#mapEvent.latlng.lat;
+    const lng = this.#mapEvent.latlng.lng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Restaurant")
+      .openPopup();
+    formContainer.classList.add("hidden");
+  }
+}
+
+const app = new App();
